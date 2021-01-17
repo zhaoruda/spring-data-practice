d243aeb4c1f5a1e12501cc2d0c8771af50e56bb8
Ruda <rdzhao@thoughtworks.com>
2021-01-17 21:34:42 +0800

    init project

---
+++ src/main/resources/db/migration/V1__create_user_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `version` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
---
+++ src/test/java/com/springdata/practice/PracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springdata.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
